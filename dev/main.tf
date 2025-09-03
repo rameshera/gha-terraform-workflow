b629bf04509667ec62fa3b3710a4b5171c631139
Hari Kammana <hari.kammana@gmail.com>
2025-09-03 14:24:42 +0530

    remote s3 backend

--- dev/main.tf
+++ dev/main.tf
@@ -2,6 +2,14 @@ provider "aws" {
   region = var.aws_region
 }
 
+terraform {
+  backend "s3" {
+    bucket = "gha-terraform-jhc"
+    key = "dev/terraform.tfstate"
+    region = "ap-south-1"
+  }
+}
+
 module "vpc" {
   source   = "../modules/vpc"
   name     = var.name
--- prod/main.tf
+++ prod/main.tf
@@ -2,6 +2,15 @@ provider "aws" {
   region = var.aws_region
 }
 
+terraform {
+  backend "s3" {
+    bucket = "gha-terraform-jhc"
+    key = "prod/terraform.tfstate"
+    region = "ap-south-1"
+  }
+}
+
+
 module "vpc" {
   source   = "../modules/vpc"
   name     = var.name
